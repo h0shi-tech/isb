fa0145b9e7475c6ce1427c4dde98daec56ca41bd
vlad <56499563+h0shi-tech@users.noreply.github.com>
2025-05-14 08:44:23 +0400

    Add files via upload

---
+++ nist_constants.py
@@ -0,0 +1,9 @@
+BLOCK_SIZE = 8
+EXPECTED_VALUES = [0.2148, 0.3672, 0.2305, 0.1875]
+SIGNIFICANCE_LEVEL = 0.01
+RUN_CATEGORIES = {
+    "<=1": 0,  
+    "2": 1,    
+    "3": 2,    
+    ">3": 3    
+} 
