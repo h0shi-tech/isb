46ae0cba723942af3d191c9741c869414020ee8f
vlad <56499563+h0shi-tech@users.noreply.github.com>
2025-05-15 12:25:06 +0400

    Update asymmetric_crypto.py

--- asymmetric_crypto.py
+++ asymmetric_crypto.py
@@ -1,5 +1,5 @@
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+import config
 
 class AsymmetricCrypto:
     def __init__(self):
@@ -8,9 +8,10 @@ class AsymmetricCrypto:
 
     def generate_keys(self):
         """Генерация пары ключей RSA"""
+        config_data = config.load_config()
         self.private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
+            public_exponent=config_data['asymmetric']['public_exponent'],
+            key_size=config_data['asymmetric']['key_size']
         )
         self.public_key = self.private_key.public_key()
         return self.private_key, self.public_key
@@ -19,20 +20,12 @@ class AsymmetricCrypto:
         """Шифрование данных открытым ключом RSA"""
         return self.public_key.encrypt(
             data,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
+            config.get_rsa_padding()
         )
 
     def decrypt(self, encrypted_data):
         """Дешифрование данных закрытым ключом RSA"""
         return self.private_key.decrypt(
             encrypted_data,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
+            config.get_rsa_padding()
         ) 
